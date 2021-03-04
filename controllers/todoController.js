aa65b0beb22306656f3329bbb544091e07925457
shahriar <30356865+sshahriar@users.noreply.github.com>
2021-03-04 14:07:46 +0600

    Add files via upload

---
+++ controllers/todoController.js
@@ -0,0 +1,22 @@
+modul.exports =  function(app)  {
+
+app.get(  '/todo' , function(req, res){
+
+
+
+}) ; 
+app.post(  '/todo' , function(req, res){
+
+
+
+}) ; 
+app.delete(  '/todo' , function(req, res){
+
+
+
+}) ; 
+
+
+
+
+} ; 
